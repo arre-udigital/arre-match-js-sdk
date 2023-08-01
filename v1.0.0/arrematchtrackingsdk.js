d379ef0649246c8f0e033f67753a18a432aa544d
Shrejal-Joshi <shrejalj10@gmail.com>
2023-08-01 18:11:23 +0530

    updated sdk

--- v1.0.0/arrematchtrackingsdk.js
+++ v1.0.0/arrematchtrackingsdk.js
@@ -28,22 +28,35 @@
     }
 
     function sendAPIRequest(utmParams) {
-        // API call using fetch:
-        // fetch('', {
-        //   method: 'POST',
-        //   body: JSON.stringify(utmParams),
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //   },
-        // })
-        //   .then((response) => response.json())
-        //   .then((data) => console.log(data))
-        //   .catch((error) => console.error('Failed:', error));
+    }
+
+    function trackUTMParameters() {
+        var utmParams = getUTMParams(window.location.href);
+
+        if (utmParams.utm_source === 'arrematch') {
+            sendAPIRequest(utmParams);
+        }
+
+        logUTMParamsToConsole(utmParams); // Log UTM parameters to the console
+        localStorage.setItem('arrematch_utm_params',JSON.stringify(utmParams));
+    }
+
+    function retrieveUTMParametersFromStorage() {
+        var storedParams = localStorage.getItem('arrematch_utm_params');
+        if (storedParams) {
+            return JSON.parse(storedParams);
+        }
+        return null;
     }
 
     function printCookieInformation() {
-        var cookie = document.cookie;
-        console.log('Cookie:',cookie);
+        var utmParams = retrieveUTMParametersFromStorage();
+        if (utmParams) {
+            console.log('Information in the cookie:',utmParams);
+        }
+    }
+
+    function markConversion() {
     }
 
     function initializeSDK() {
@@ -57,48 +70,15 @@
             throw new Error(JSON.stringify(errorResponse));
         }
 
-        var pageRelativeUrl = window.location.pathname;
-        var domainName = window.location.hostname;
-        var userAgent = navigator.userAgent;
-        var vendor = navigator.vendor;
-        var platform = navigator.platform;
-
-        console.log('Page Relative URL:',pageRelativeUrl);
-        console.log('Domain Name:',domainName);
-        console.log('User Agent:',userAgent);
-        console.log('Vendor:',vendor);
-        console.log('Platform:',platform);
-
-        // Store UTM parameters in local storage
-        var utmParams = getUTMParams(window.location.href);
-        localStorage.setItem('utmParams',JSON.stringify(utmParams));
-
-        logUTMParamsToConsole(utmParams);
         setTimeout(trackUTMParameters,0);
     }
 
-    function trackUTMParameters() {
-        var utmParams = getUTMParams(window.location.href);
-
-        if (utmParams.utm_source === 'arrematch') {
-            sendAPIRequest(utmParams);
-        }
-
-        logUTMParamsToConsole(utmParams);
-    }
-
-    function markConversion() {
-        // Perform the validation check to ensure a single sessionId can have only one conversion event
-        // If the validation passes, make the API call with isConversionEvent as true
-        //   Perform a validation check to ensure that a single sessionId can have only one conversion event.
-        //   If that already exists, ignore all the other API requests received in such way because it may be due to a bug
-        // in the client installation.
-    }
-
+    arrematch.logUTMParamsToConsole = logUTMParamsToConsole;
     arrematch.initializeSDK = initializeSDK;
     arrematch.markConversion = markConversion;
     arrematch.printCookieInformation = printCookieInformation;
 
+    // Expose the arrematch namespace globally
     window.arrematch = arrematch;
 
     arrematch.initializeSDK();
